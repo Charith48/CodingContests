7a723bac5035f8d5f89457ddeb81fae0840da6bd
charith babu <rcharithbabu@gmail.com>
2023-03-03 19:06:23 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+let imgSrc=[
+    "https://contentstatic.techgig.com/photo/88751917/7-programming-languages-every-beginner-should-explore.jpg?35120",
+    "https://6448316.fs1.hubspotusercontent-na1.net/hubfs/6448316/what-computer-programming-jobs-offer-remote-work-jpg.jpeg",
+    "https://assets.entrepreneur.com/content/3x2/2000/1634062199-Ent-Python.jpg",
+    "https://res.cloudinary.com/grand-canyon-university/image/fetch/w_750,h_564,c_fill,g_faces,q_auto/https://www.gcu.edu/sites/default/files/2020-09/programming.jpg",
+    "https://cdn.columbiauniversitybootcamp.com/wp-content/uploads/sites/108/2020/10/CDG_blog_post_image_02-850x412.jpg",
+    "https://www.codecademy.com/resources/blog/content/images/2020/07/what-is-a-programming-language.png",
+    "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2022/02/best-programming-languages-to-learn.png",
+    "https://appinventiv.com/wp-content/uploads/sites/1/2019/02/18-Programming-Languages-Redefining-The-Future-Of-Coding-scaled.webp",
+    "https://www.herzing.edu/sites/default/files/styles/fp_960_480/public/2020-09/it_computer_programming.jpg.webp?itok=AKSaSh2O",
+    "https://cdn.ucberkeleybootcamp.com/wp-content/uploads/sites/106/2020/12/tes_gen_blog_code6-800x412.jpg",
+    "https://www.forbes.com/advisor/wp-content/uploads/2022/06/computer_programming.jpeg.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq2GVrs-iS1Xc6WEHvxfoKOE-tceJ24_7s-qNwSbl5zVDDiJG7NqB6bzoXCjCuLQhZELI&usqp=CAU",
+    "https://media.istockphoto.com/id/1372353160/photo/young-asian-programmer-working-at-office.jpg?s=170667a&w=0&k=20&c=Os_LuQEyrPxo6ctN5KUnSaorw5nr8iy6_lLzBpqXSjI=",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE0DeOwF4LwqSR21nbPQIbtWw65K5utf7RrQ&usqp=CAU",
+    "https://cdn.dubootcamp.com/wp-content/uploads/sites/66/2020/05/shutterstock_753780223-850x412.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgu8jxazBk9GYNnWR6Erq8zmN0ltD2lBXxt--8IeS_JC89PzeFJWa_zsQDOjNi-pWUV70&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3QqwASAnp1azq6J-xjB1DF27yZB7zIQn2zMdmz0uuT_AgJlhOCHs3Wpr38YE0M1xP9qs&usqp=CAU",
+    "https://assets.gocoderz.com/site/wp-content/uploads/2021/05/My-Post-42-1024x683.png",
+    "https://extension.harvard.edu/wp-content/uploads/sites/8/2020/10/computer-programming.jpg",
+    "https://media.wired.com/photos/5ca7e7fce903a149797c4516/master/pass/Ideas_Art_Languages-899643860.jpg",
+    "https://news.uchicago.edu/sites/default/files/images/image/20170906/programming.jpg",
+    "https://www.analyticssteps.com/backend/media/thumbnail/1084449/5171540_1628741951_Top%20programming%20languages%20for%20machine%20learningArtboard%201.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLpHUiOUi93SCUBa-GlcPrM7PSVfcR5XAvqA&usqp=CAU",
+    "https://149695847.v2.pressablecdn.com/wp-content/uploads/2021/07/Untitled-design-19.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5y-8oSjZJ-_xnbGaTm7ifkxbRWF0LGhLe2INDOi-G7IdJKUO19mEf6uyJzsqxc3O3F1A&usqp=CAU",
+    "https://www.forbes.com/advisor/wp-content/uploads/2022/07/video_game.jpeg.jpg",
+    "https://media.istockphoto.com/id/1197071023/photo/portrait-of-software-developer-hacker-gamer-wearing-glasses-and-headset-sitting-at-his-desk.jpg?s=612x612&w=0&k=20&c=Xr-KykAag1nnR0I7YC69jRfkmAgoSUZx6d9vQ5OsF28=",
+    "https://cdn.mos.cms.futurecdn.net/9QTpESGBXa32D29J77VR3d.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQczuS821pTVLwvcaW2P1JJV0PMwzgy43q3Tg&usqp=CAU",
+    "https://www.freecodecamp.org/news/content/images/2021/10/oskar-yildiz-gy08FXeM2L4-unsplash.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM7RGx2JMXpnXCqI-KlUZ4yxkqkEr9frpaWQ&usqp=CAU",
+    "https://contentstatic.techgig.com/photo/88751917/7-programming-languages-every-beginner-should-explore.jpg?35120",
+    "https://6448316.fs1.hubspotusercontent-na1.net/hubfs/6448316/what-computer-programming-jobs-offer-remote-work-jpg.jpeg",
+    "https://assets.entrepreneur.com/content/3x2/2000/1634062199-Ent-Python.jpg",
+    "https://res.cloudinary.com/grand-canyon-university/image/fetch/w_750,h_564,c_fill,g_faces,q_auto/https://www.gcu.edu/sites/default/files/2020-09/programming.jpg",
+    "https://cdn.columbiauniversitybootcamp.com/wp-content/uploads/sites/108/2020/10/CDG_blog_post_image_02-850x412.jpg",
+    "https://www.codecademy.com/resources/blog/content/images/2020/07/what-is-a-programming-language.png",
+    "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2022/02/best-programming-languages-to-learn.png",
+    "https://appinventiv.com/wp-content/uploads/sites/1/2019/02/18-Programming-Languages-Redefining-The-Future-Of-Coding-scaled.webp",
+    "https://www.herzing.edu/sites/default/files/styles/fp_960_480/public/2020-09/it_computer_programming.jpg.webp?itok=AKSaSh2O",
+    "https://cdn.ucberkeleybootcamp.com/wp-content/uploads/sites/106/2020/12/tes_gen_blog_code6-800x412.jpg",
+    "https://www.forbes.com/advisor/wp-content/uploads/2022/06/computer_programming.jpeg.jpg",
+    "https://www.cwjobs.co.uk/advice/wp-content/uploads/programmer-job-description-1024x576.jpg"
+]
+let container=document.querySelector(".codeContestBlock");
+let uri="https://kontests.net/api/v1/all";
+let p=fetch(uri);
+p.then((response)=>{
+    return response.json();
+}).then((response)=>{
+    response.forEach((e,i)=>{
+        container.innerHTML+=`
+    <div class="card">
+        <img src="${imgSrc[i]}" alt="image">
+        <div class="info">
+            <h3>${e.name}</h3>
+            <p><span class="infoContent">START TIME</span>&nbsp;:&nbsp;&nbsp;${e.start_time}</p>
+            <p><span class="infoContent">END TIME</span>&nbsp;:&nbsp;&nbsp;${e.end_time}</p>
+            <p><span class="infoContent">DURATION</span>&nbsp;:&nbsp;&nbsp;${e.duration}</p>
+            <p><span class="infoContent">IN 24 HOURS</span>&nbsp;:&nbsp;&nbsp;${e.in_24_hours}</p>
+            <p><span class="infoContent">STATUS</span>&nbsp;:&nbsp;&nbsp;${e.status}<p>
+            <p><span class="infoContent">CONDUCTED BY</span>&nbsp;:&nbsp;&nbsp;${e.site}</p>            
+        </div>
+        <div class="visit">
+        <a href="${e.url}">Visit now </a>
+        </div>
+    </div>
+        `
+    })
+    console.log(response);
+});
+
